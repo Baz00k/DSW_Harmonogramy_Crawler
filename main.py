54616d3e87cd56b9317e622920dd93a9f430ab56
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2022-10-20 10:57:02 +0200

    change imports

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from crawler.crawler import Crawler
-import data_extractor.extractor as extractor
+from extractor.extractor import extract_data_to_csv
 
 def main():
     groupID = 8931
     website_url = f'https://harmonogramy.dsw.edu.pl/Plany/PlanyGrup/{groupID}'
 
-    crawler = Crawler()
-    crawler.load_page(website_url)
-    crawler.load_table_data()
-    html = crawler.get_page_source()
-
-    extractor.extract_data_to_csv(html, f'group_{groupID}')
+    dsw_crawler = Crawler()
+    dsw_crawler.load_page(website_url)
+    dsw_crawler.load_table_data()
+    html = dsw_crawler.get_page_source()
+    
+    extract_data_to_csv(html, f'group_{groupID}')
 
 
 if __name__ == '__main__':
