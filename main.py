6e1290340ca4c1b0bf280223c05585c1e2825fa7
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2022-10-20 11:12:19 +0200

    crawl range, handle exeptions

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from crawler.crawler import Crawler
 from extractor.extractor import extract_data_to_csv
 
 def main():
-    groupID = 8931
-    website_url = f'https://harmonogramy.dsw.edu.pl/Plany/PlanyGrup/{groupID}'
+    dsw_crawler = Crawler(headless=True)
 
-    dsw_crawler = Crawler()
-    dsw_crawler.load_page(website_url)
-    dsw_crawler.load_table_data()
-    html = dsw_crawler.get_page_source()
-    
-    extract_data_to_csv(html, f'group_{groupID}')
+    for groupID in range(8900, 8950):
+        website_url = f'https://harmonogramy.dsw.edu.pl/Plany/PlanyGrup/{groupID}'
+        try:
+            dsw_crawler.load_page(website_url)
+            dsw_crawler.load_table_data()
+            html = dsw_crawler.get_page_source()
+            extract_data_to_csv(html, f'group_{groupID}')
+        except:
+            print(f'Group {groupID} not found')
 
 
 if __name__ == '__main__':
